7b6582bc927143eb011571e1f0e6ac8910a0c410
Kundan Kumar <69016025+kundan726@users.noreply.github.com>
2021-09-10 01:50:46 +0530

    Add files via upload

---
+++ day 2/Multidimensional_array.java
@@ -0,0 +1,56 @@
+package com.codeWithHarry;
+import java.util.Arrays;
+import java.util.*;
+
+public class Multidimensional_array {
+    public static void main(String[] args) {
+        int [][] house=new int[2][3];
+        house[0][0]=101;
+        house[0][1]=102;
+        house[0][2]=101;
+        house[1][0]=201;
+        house[1][1]=202;
+        house[1][2]=203;
+        
+
+        System.out.println("enter the elements for an array");
+        for (int i=0;i<2;i++){
+            for (int j=0;j<3;j++){
+                System.out.print(house[i][j] + "\t");
+//                System.out.println(" ");
+            }
+            System.out.println(" ");
+        }
+
+
+// Taking input from user
+        System.out.println("Enter the size of an array");
+        Scanner sc=new Scanner(System.in);
+        int i,j;
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+        int arr[][]=new int[m][n];
+//        System.out.println(arr[m][n]);
+        System.out.println("Enter The contents of row and column");
+        //Loop for entering the elememts of an array
+
+        for ( i=0;i<m;i++){
+            for ( j = 0; j < n; j++) {
+                arr[i][j]=sc.nextInt();
+//
+            }
+//
+        }
+
+        /*Loop for printing the elements of and but their is an error in this part that needs to be
+        corrected
+         */
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j <n ; j++) {
+                System.out.println(arr[i][j]);
+            }
+        }
+
+    }
+}
